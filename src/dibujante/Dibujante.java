d6bd42d841329b021df42407b692fdf95662893b
Mariano Castillo <Mariano Castillo@DESKTOP-9NFL535.mshome.net>
2020-11-14 18:59:19 -0500

    Establece Nimbus como LAF de la aplicacion

--- src/dibujante/Dibujante.java
+++ src/dibujante/Dibujante.java
@@ -7,8 +7,19 @@ package dibujante;
  */
 
 public class Dibujante {
- 
-  public static void main(String[] args) {
-     new VentanaPrincipal().setVisible(true);
-  }
+
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        new VentanaPrincipal().setVisible(true);
+    }
 }
